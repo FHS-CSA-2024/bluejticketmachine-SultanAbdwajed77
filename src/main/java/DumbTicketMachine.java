5aabe90ed2a23e2639247d69f00873c5b9b6fe2d
SultanAbdwajed77 <wajedfiroz7@gmail.com>
2024-09-06 13:58:47 -0700

    Update DumbTicketMachine.java

--- src/main/java/DumbTicketMachine.java
+++ src/main/java/DumbTicketMachine.java
@@ -11,50 +11,71 @@ package src.main.java;
  * @author David J. Barnes and Michael Kölling
  * @version 2016.02.29
  */
-public class DumbTicketMachine{
+public class DumbTicketMachine
+{
+    // The price of a ticket from this machine.
     private int price;
+    // The amount of money entered by a customer so far.
     private int balance;
-    private int total;
+    // The total amount of money collected by this machine.
+    private int total;   
 
-    public DumbTicketMachine(int cost){
+    /**
+     * Create a machine that issues tickets of the given price.
+     * Note that the price must be greater than zero, and there
+     * are no checks to ensure this.
+     */
+    public DumbTicketMachine(int cost)
+    {
         price = cost;
         balance = 0;
         total = 0;
     }
-    
-    public DumbTicketMachine(){
-        price = 50; // Default price
-        balance = 0;
-        total = 0;
-    }
 
-    public int getPrice(){
+    /**
+     * Return the price of a ticket.
+     */
+    public int getPrice()
+    {
         return price;
     }
 
-    public int getBalance(){
+    /**
+     * Return the amount of money already inserted for the
+     * next ticket.
+     */
+    public int getBalance()
+    {
         return balance;
     }
 
-    public void insertMoney(int amount){
-        balance += amount;
+    /**
+     * Receive an amount of money from a customer.
+     */
+    public void insertMoney(int amount)
+    {
+        balance = balance + amount;
     }
 
-    public void printTicket(){
+    /**
+     * Print a ticket.
+     * Update the total collected and
+     * reduce the balance to zero.
+     */
+    public void printTicket()
+    {
+        // Simulate the printing of a ticket.
         System.out.println("##################");
         System.out.println("# The BlueJ Line");
         System.out.println("# Ticket");
         System.out.println("# " + price + " cents.");
         System.out.println("##################");
         System.out.println();
-        
-        total += balance;
-        balance = 0;
-    }
 
-    public void empty(){
-        total = 0;
+        // Update the total collected with the balance.
+        total = total + balance;
+        // Clear the balance.
         balance = 0;
-        System.out.println("The ticket machine has been emptied.");
+
     }
 }
