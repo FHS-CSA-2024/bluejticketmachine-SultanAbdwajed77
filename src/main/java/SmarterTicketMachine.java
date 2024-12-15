38c580e9baeac05a370f8e85b333d5d1472ea8b0
SultanAbdwajed77 <wajedfiroz7@gmail.com>
2024-12-14 23:07:46 -0800

    Update SmarterTicketMachine.java

--- src/main/java/SmarterTicketMachine.java
+++ src/main/java/SmarterTicketMachine.java
@@ -1,58 +1,72 @@
 package src.main.java;
 
 public class SmarterTicketMachine {
-   private int price;  
-   private int balance;  
-   private int total;  
+    // The price of a ticket from this machine.
+    private int price;
+    // The amount of money entered by a customer so far.
+    private int balance;
+    // The total amount of money collected by this machine.
+    private int total;
 
-   public SmarterTicketMachine(int cost) {  
-      price = cost;  
-      balance = 0;  
-      total = 0;  
-   }  
+    // Constructor that takes the price as a parameter
+    public SmarterTicketMachine(int cost) {
+        price = cost;
+        balance = 0;
+        total = 0;
+    }
 
-   public int getPrice() {  
-      return price;  
-   }  
+    // Method to return the price of a ticket
+    public int getPrice() {
+        return price;
+    }
 
-   public int getBalance() {  
-      return balance;  
-   }  
+    // Method to return the amount of money already inserted for the next ticket
+    public int getBalance() {
+        return balance;
+    }
 
-   public void insertMoney(int amount) {  
-      if (amount > 0) {  
-        balance += amount;  
-      } else {  
-        System.out.println("Enter a positive amount.");
-      }  
-   }  
+    // Method to receive an amount of money from a customer
+    public void insertMoney(int amount) {
+        if (amount > 0) {
+            balance += amount;
+        } else {
+            System.out.println("Enter a positive amount.");
+        }
+    }
 
-   public void printTicket() {  
-      int amountLeftToPay = price - balance;  
-      if (balance >= price) {  
-        System.out.println("##################");  
-        System.out.println("# The BlueJ Line");  
-        System.out.println("# Ticket");  
-        System.out.println("# " + price + " cents.");  
-        System.out.println("##################");  
-        System.out.println();  
-        total += price;  
-        balance -= price;  
-      } else {  
-        System.out.println("You must insert at least: " + amountLeftToPay + " more cents.");  
-      }  
-   }  
+    // Method to print a ticket
+    public void printTicket() {
+        int amountLeftToPay = price - balance;
+        if (balance >= price) {
+            // Simulate the printing of a ticket.
+            System.out.println("##################");
+            System.out.println("# The BlueJ Line");
+            System.out.println("# Ticket");
+            System.out.println("# " + price + " cents.");
+            System.out.println("##################");
+            System.out.println();
 
-   public int refundBalance() {  
-      int amountToRefund = balance;  
-      balance = 0;  
-      return amountToRefund;  
-   }  
+            // Update the total collected with the price.
+            total += price;
+            // Reduce the balance by the price.
+            balance -= price;
+        } else {
+            System.out.println("You must insert at least: " + amountLeftToPay + " more cents.");
+        }
+    }
 
-   public int emptyMachine() {  
-      int amountInTotal = total;  
-      total = 0;  
-      System.out.println("The ticket machine has been emptied.");  
-      return amountInTotal;  
-   }  
+    // Method to refund the balance (return the current balance)
+    public int refundBalance() {
+        int amountToRefund = balance;
+        balance = 0;
+        return amountToRefund;
+    }
+
+    // Method to empty the machine (removes all money from the machine)
+    public int emptyMachine() {
+        int amountInTotal = total;
+        total = 0;
+        System.out.println("The ticket machine has been emptied.");
+        return amountInTotal;
+    }
 }
